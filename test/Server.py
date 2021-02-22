c75b49ab0d5702ffb9329abf357258b9fbe8ea01
Jourdelune <jourdelune863@gmail.com>
2021-02-22 15:11:21 +0100

    yes

---
+++ test/Server.py
@@ -0,0 +1,58 @@
+from quart import Quart, redirect, url_for, render_template, g, request
+from quart_discord import DiscordOAuth2Session, requires_authorization, Unauthorized
+from discord.ext.ipc import Client
+
+app = Quart(__name__)
+web_ipc = Client(host="localhost", port=8765, secret_key="secret_key")
+
+app.secret_key = b"random bytes representing quart secret key"
+
+app.config["DISCORD_CLIENT_ID"] = 805082505320333383   
+app.config["DISCORD_CLIENT_SECRET"] = "iQrq77S2qOakW4BR7XS7G4qKnKQ0TuDS"              
+app.config["DISCORD_REDIRECT_URI"] = "http://127.0.0.1:5000/callback"                
+app.config["DISCORD_BOT_TOKEN"] = "ODA1MDgyNTA1MzIwMzMzMzgz.YBVtgg.ie3BSi7q6z2SmEKSymLuA4mNj4Y"    
+
+discord = DiscordOAuth2Session(app)
+
+
+@app.route("/login/")
+async def login():
+    return await discord.create_session(scope=["identify", "guilds"])
+
+
+
+@app.route("/callback/")
+async def callback():
+    await discord.callback()
+    return redirect(url_for(".me"))
+
+
+@app.errorhandler(Unauthorized)
+async def redirect_unauthorized(e):
+    return redirect(url_for("login"))
+
+
+@app.route("/me/")
+@requires_authorization
+async def me():
+    user = await discord.fetch_user()
+    common_guild = []
+    no_common_guild = []
+    icon_guild = {}
+    
+    guilds = await discord.fetch_guilds()
+    guild_list = await web_ipc.request("get_guild_list")
+    for i in guilds:
+        if i.is_owner:   
+            if (str(i.id) in guild_list):
+                common_guild.append(i)
+            else:
+                no_common_guild.append(i)
+                
+            
+   
+    return await render_template('index.html', user=user, guilds=common_guild, no_guild=no_common_guild)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ test/ipcroutes.py
@@ -0,0 +1,37 @@
+# BOT FILE
+import discord
+from discord.ext import commands
+
+from discord.ext.ipc import Server
+
+class Bot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    async def on_ipc_ready(self):
+        print("IPC ready")
+    
+    async def on_ready(self):
+        print("Bot ready")
+
+bot = Bot(command_prefix="!", case_insensitive=True)
+bot_ipc = Server(bot, "localhost", 8765, "secret_key")
+
+
+
+@bot_ipc.route() 
+async def get_guild_count(data):
+    return len(bot.guilds)
+
+@bot_ipc.route() 
+async def get_guild_list(data):
+    list_id = []
+    for i in bot.guilds:
+        list_id.append(i.id)
+        
+    return list_id
+        
+
+if __name__ == "__main__":
+    bot_ipc.start()
+    bot.run("ODA1MDgyNTA1MzIwMzMzMzgz.YBVtgg.ie3BSi7q6z2SmEKSymLuA4mNj4Y"   )
