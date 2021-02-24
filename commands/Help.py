ee0d298a0eaedc25e652b76530936fa210777dc5
Jourdelune <jourdelune863@gmail.com>
2021-02-24 13:09:46 +0100

    2.0

--- commands/Help.py
+++ commands/Help.py
@@ -25,7 +25,7 @@ class Volume(commands.Cog):
         
         if arg == None:
             embed = discord.Embed(color=embed_color(ctx.guild.id), title="Song's Bot help page", description=f"""Song's Bot is a music bot **easy to use** with **a dashboard** and **a customizable behavior**.\n
-            **[Dashboard link](https://lol)\n[Documentation link](https://lol2)\n[Vote link](https://lol2)\n[Bot invite](https://lol2)\n\n**""")
+            **[Dashboard link](http://songs-bot.tk/me)\n[Documentation link](http://songs-bot.tk/commands)\n[Vote link](https://top.gg)\n[Bot invite](https://discord.com/oauth2/authorize?client_id=805082505320333383&scope=bot&permissions=70634560)\n\n**""")
             embed.add_field(name="**:notes: Music**", value=f"`play` `replay` `skip` `stop` `pause` `resume` `loop` `queue` `volume` `np` `leave` `join` `remove` `shuffle`\n\n", inline=False)
             embed.add_field(name="**:computer: Song's Bot**", value=f"`invite` `about` `support` `vote` `ping` `help`\n\n", inline=False)
             embed.add_field(name="**:tools: Gestion**", value=f"**[Go to Dashboard](https://lol)**\n", inline=False)
